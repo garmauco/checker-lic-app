f5f3a66f00eee27da2bd06c48d5a0f40ebb434e5
garmauco <garmauco@gmail.com>
2023-05-04 14:13:40 -0500

    fix function validate

--- src/CheckerLicApp.php
+++ src/CheckerLicApp.php
@@ -16,10 +16,19 @@ class CheckerLicApp implements LicenseValidatorInterface
 
     public function validate(string $licenseKey): bool
     {
-        // Aquí puedes implementar la lógica de validación de la clave de licencia.
-        // Por ejemplo:
-        $validLicenseKey = 'tu_clave_de_licencia_correcta';
-        return $licenseKey === $validLicenseKey;
+        $client = new Client(['http_errors' => false]);
+        $response = $client->get('https://api-verification.3mas1r.com/validate_license.php?license_key=' . $licenseKey);
+
+        $statuscode = $response->getStatusCode();
+        $response = json_decode($response->getBody(), true);
+
+        if ($statuscode === 200) {
+            return $response['is_valid'];
+        } elseif ($statuscode === 404) {
+            return false;
+        } else {
+            return false;
+        }
     }
 
     public function validateByProjectUrl(string $projectUrl): bool
@@ -42,6 +51,5 @@ class CheckerLicApp implements LicenseValidatorInterface
         return isset($licenseStatus['is_valid']) && $licenseStatus['is_valid'] == true;
     }
 }
-
 
 
